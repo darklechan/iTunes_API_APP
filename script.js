0ff8716c672b7fe400e24afced176a5f2deee8da
Valeria Escalante <darklechan92@gmail.com>
2021-03-04 17:50:01 +0100

    tracklist added

--- script.js
+++ script.js
@@ -3,15 +3,32 @@ let button = document.getElementById("submit");
 let input = document.getElementById("input_text");
 let output_list = document.getElementById("output_list");
 
+
 //añado un evento al botón de buscar:
 button.addEventListener("click", getDataFromApi);
 
 function getDataFromApi() {
   const url = "https://itunes.apple.com/search?term=" + input.value;
-//   console.log(url);
-  fetch("https://itunes.apple.com/search?term=" + input.value)
+  fetch(url)
   .then((response) => response.json())
-  .then((json) => console.log(json))
-  .catch((error) => console.log(error));
+  .then(data => {
+    let html = "";
+    console.log("esto es lo k hay: ",data);
+    data.results.forEach(song => {
+          //hay que mostrar el título y el autor
+          html += `
+              <li class="collection-item avatar">
+                <img src=${song.artworkUrl60} alt="" class="circle">
+                <span class="title">${song.artistName}</span>
+                <p>${song.trackName}<br>
+                </p>
+              </li>
+          `;
+        })
+      
+      
+      output_list.innerHTML = html;
+    });
 }
-// console.log(input.value);
+
+// .catch((error) => console.log(error));
