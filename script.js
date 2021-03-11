1a19e642c134b99f070fc31d47e25ad56c4e1394
Valeria Escalante <darklechan92@gmail.com>
2021-03-11 19:20:58 +0100

    delete function added, autoplay added

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function getDataFromApi() {
     .then((json) => {
       const data = json;
       renderData(data);
-      console.log("esto es lo k hay: ", data);
+      // console.log("esto es lo k hay: ", data);
     });
   }
   
@@ -34,14 +34,14 @@ function getDataFromApi() {
     };
   }
 
-  let touchStartX = 0;
-  let touchEndX = 0;
   
   /*
   Con esta función hago el display de los datos de getDataFromApi
   y los muestro en una lista
   */
  function renderData(data) {
+   let touchStartX = 0;
+   let touchEndX = 0;
    output_list.innerHTML = "";
    data.results.forEach((song) => {
     //hay que mostrar el título y el autor
@@ -53,65 +53,65 @@ function getDataFromApi() {
       `;
     li.addEventListener("click", (e) => {
       li.style.backgroundColor = "grey";
-
       // console.log("que color es:",li.classList.remove(changeColor)); //esto en teoria tiene que borrar el color :/
       // console.log("sibling?", li.nextSibling);
-      showMediaPlayer(song); //esto es lo mismo que ponerlo en el html ?
+      showMediaPlayer(song); //esto es lo mismo que ponerlo en el html 
     });
-
     output_list.appendChild(li);
-    // li.addEventListener('swipe-left', function(e) {
-    //   console.log(e.target);
-    //   li.remove();
 
-    // });
+    //swipe and remove:
     li.addEventListener("touchstart", e => {
       touchStartX = e.changedTouches[0].screenX;
     });
     li.addEventListener("touchend", e => {
       touchEndX = e.changedTouches[0].screenX;
-      swipeDelete();
-      li.remove();
+      if (touchEndX < touchStartX) {
+        // alert('¿Deseas borrar esa canción?');
+        let removeButton = "";
+          removeButton += `
+            <button class="waves-effect btn-small">Delete</button>
+          `;
+        // li.remove(); 
+        
+      } else {
+        alert('swipe left bitch');
+      }
+      
     });
   });
-}
-
-function swipeDelete() {
-  if(touchEndX < touchStartX) alert('¿Deseas borrar esa canción?');  
-  if(touchEndX > touchStartX) alert('¿Deseas borrar esa canción?');  
 }
 
 function showMediaPlayer(song) {
   let html = "";
   if (song.kind === "feature-movie" || song.kind === "music-video") {
     html += `
-    <li class="collection-item">
-      <img src="${song.artworkUrl60}" alt="" class="circle">
-      <span class="title">${song.artistName} - ${song.trackName}</span><br><br>
-      <video controls autoplay width="300" height="auto" name="media"><source src="${song.previewUrl}" type="audio/x-m4a"</video>
+    <li class="collection-item" id="tracklist">
+    <img src="${song.artworkUrl60}" alt="" class="circle">
+    <span class="title" id="artistname">${song.artistName} - ${song.trackName}</span><br><br>
+    <video controls autoplay id="player" width="300" height="auto" name="media"><source src="${song.previewUrl}" type="audio/x-m4a"</video>
     </li>
-  `;
-    // let songs = [song.previewUrl];
-    // console.log(songs.sort);
+    `;
     media_player.innerHTML = html;
-    // let hola = document.querySelector("video");
-    // hola.addEventListener("ended", (e) => {
-    //   let position = e[0];
-    //   position++;
-    //   hola.play();
-    // });
   } else if (song.wrapperType === "audiobook") {
     let html = "";
     media_player.innerHTML = html;
   } else {
     html += `
-      <li class="collection-item">
-        <img src="${song.artworkUrl60}" alt="" class="circle">
-        <span class="title">${song.artistName} - ${song.trackName}</span><br><br>
-        <video controls autoplay width="300" height="50" name="media"><source src="${song.previewUrl}" type="audio/x-m4a"</video>
-      </li>
+    <li class="collection-item" id="tracklist">
+    <img src="${song.artworkUrl60}" alt="" class="circle">
+    <span class="title" id="artistname">${song.artistName} - ${song.trackName}</span><br><br>
+    <video controls autoplay id="player" width="300" height="50" name="media"><source src="${song.previewUrl}" type="audio/x-m4a"</video>
+    </li>
     
     `;
     media_player.innerHTML = html;
+    var nextsrc = song.previewUrl;
+    var player = document.getElementById("player");
+    console.log(player);
+    player.onended = function() {
+      for (var i = 0; i < nextsrc.length; i++) {
+        player.play(nextsrc[i]); 
+      }
+    }
   }
 }
