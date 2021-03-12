c22265749b05335fcc7030e4e5268720b53ac576
Valeria Escalante <darklechan92@gmail.com>
2021-03-12 19:41:34 +0100

    final details added - sorting still not working

--- script.js
+++ script.js
@@ -19,64 +19,82 @@ function getDataFromApi() {
     .then((json) => {
       const data = json;
       renderData(data);
-      // console.log("esto es lo k hay: ", data);
     });
-  }
-  
-  function sortFilesByType(kind) {
-    return function (a, b) {
-      if (a[kind] > b[kind]) {
-        return 1;
-      } else if (a[kind] < b[kind]) {
-        return -1;
-      }
-      return 0;
-    };
-  }
+}
 
-  
-  /*
+/*
+  Con esta función he intentado hacer lo del array.sort() ya que era lo que había
+  visto en la documentación, pero al intentar añadirlo me salían problemas. No sé
+  si me he complicado mucho y en realidad es más sencillo pero como no me salía
+  he decidido dejarlo. La idea la he sacado de aquí: https://www.sitepoint.com/sort-an-array-of-objects-in-javascript/
+  Ej: data.sort(sortFilesByType("song"));
+  */
+
+// function sortFilesByType(kind) {
+//   return function (a, b) {
+//     if (a[kind] > b[kind]) {
+//       return 1;
+//     } else if (a[kind] < b[kind]) {
+//       return -1;
+//     }
+//     return 0;
+//   };
+// }
+
+/*
   Con esta función hago el display de los datos de getDataFromApi
   y los muestro en una lista
   */
- function renderData(data) {
-   let touchStartX = 0;
-   let touchEndX = 0;
-   output_list.innerHTML = "";
-   data.results.forEach((song) => {
-    //hay que mostrar el título y el autor
+function renderData(data) {
+  let touchStartX = 0;
+  let touchEndX = 0;
+  output_list.innerHTML = "";
+  data.results.forEach((song) => {
     let li = document.createElement("li"); //voy creando lis
-    li.classList.add("collection-item"); //de esta manera se crea igual que en HTML ?
+    li.classList.add("collection-item"); //de esta manera se crea igual que en HTML
     li.innerHTML += `
       <span class="center-align""><i class="small material-icons">play_arrow</i></span>
       <span class="title" id="track">${song.artistName} - ${song.trackName}</span>
       `;
-    li.addEventListener("click", (e) => {
+    li.addEventListener("click", () => {
       li.style.backgroundColor = "grey";
-      // console.log("que color es:",li.classList.remove(changeColor)); //esto en teoria tiene que borrar el color :/
-      // console.log("sibling?", li.nextSibling);
-      showMediaPlayer(song); //esto es lo mismo que ponerlo en el html 
+      if(li.previousSibling === null) {
+        console.log("no hay elemento anterior");
+      } else {
+        li.previousSibling.removeAttribute("style");
+      }
+      
+      // li.nextElementSibling.style.backgroundColor = "initial";
+      showMediaPlayer(song); //esto es lo mismo que ponerlo en el html
     });
     output_list.appendChild(li);
 
-    //swipe and remove:
-    li.addEventListener("touchstart", e => {
+    /*
+    En esta parte empieza el evento del swipe, funciona un poco de aquella manera
+    pero no he sabido hacerlo mejor, se puede optimizar muchísimo mejor pero no
+    he lo he conseguido. Además el problema que hay es que cuando borras esa canción
+    se queda en autoplay porque es el primer evento que se ejecuta.
+    */
+    li.addEventListener("touchstart", (e) => {
       touchStartX = e.changedTouches[0].screenX;
     });
-    li.addEventListener("touchend", e => {
+    li.addEventListener("touchend", (e) => {
       touchEndX = e.changedTouches[0].screenX;
       if (touchEndX < touchStartX) {
         // alert('¿Deseas borrar esa canción?');
         let removeButton = "";
-          removeButton += `
-            <button class="waves-effect btn-small">Delete</button>
-          `;
-        // li.remove(); 
-        
-      } else {
-        alert('swipe left bitch');
+        removeButton += `
+        <button class="waves-effect btn-small" id="removeBtn">Delete</button>
+        `;
+        li.innerHTML = removeButton;
+        let removeBtn = document.getElementById("removeBtn");
+        removeBtn.addEventListener("click", () => {
+          li.remove();
+        });
+      } else if (touchEndX < touchStartX) {
+        let removeButton = "";
+        li.innerHTML = removeButton;
       }
-      
     });
   });
 }
@@ -105,13 +123,19 @@ function showMediaPlayer(song) {
     
     `;
     media_player.innerHTML = html;
+    /*
+    En este apartado he intentado hacer lo del play pero el problema es que se queda
+    en el mismo elemento y hace un loop de la canción. He probado de varias maneras
+    pero no he conseguido que pasara al siguiente elemento de la lista. Imagino que
+    en lugar de iterar sobre el player, debo iterar sobre la lista y que vaya detectando
+    el final de la canción.
+    */
     var nextsrc = song.previewUrl;
     var player = document.getElementById("player");
-    console.log(player);
-    player.onended = function() {
+    player.onended = function () {
       for (var i = 0; i < nextsrc.length; i++) {
-        player.play(nextsrc[i]); 
+        player.play(nextsrc[i]);
       }
-    }
+    };
   }
 }
